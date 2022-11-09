bbf499ed4fb54bda00b6a8251ee70da38624e237
LEE DONG GYU <88726990+tenenger@users.noreply.github.com>
2022-11-09 10:28:27 +0900

    Time: 107 ms (47.47%), Space: 42.7 MB (40.00%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.js
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -3,18 +3,6 @@
  * @return {boolean}
  */
 
-function isRemove(singleBracket, stackTop) {
-    
-    if (stackTop === null) return false;
-    
-    if (stackTop === "(" && singleBracket === ")" ||
-        stackTop === "{" && singleBracket === "}" ||
-        stackTop === "[" && singleBracket === "]"
-       ) return true
-    
-    return false
-}
-
 var isValid = function(s) {
     const stack = [];
     
