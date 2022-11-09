145392377320b035fd9d6054006e5d427b924b65
LEE DONG GYU <88726990+tenenger@users.noreply.github.com>
2022-11-09 13:27:05 +0900

    Time: 199 ms (50.89%), Space: 46.3 MB (52.96%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.js
@@ -0,0 +1,31 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+
+// 가운데 문자가 1개인 경우(홀수)
+// 가운데 문자가 2개인 경우(짝수)
+function makePalindrome(s, i, j) {
+    while (0 <= i && j <= s.length) {
+        if (s[i] !== s[j]) break;
+        
+        i--;
+        j++;
+    }
+    
+    return s.slice(i+1, j);
+}
+// 뒤에서 부터 접근하면 더 빠를듯?
+var longestPalindrome = function(s) {
+    let answer = '';
+    
+    for (let i=0; i<s.length; i++) {
+        const case1 = makePalindrome(s, i, i);
+        const case2 = makePalindrome(s, i, i+1);
+        
+        if (answer.length < case1.length) answer = case1;
+        if (answer.length < case2.length) answer = case2;
+    }
+    
+    return answer
+};
