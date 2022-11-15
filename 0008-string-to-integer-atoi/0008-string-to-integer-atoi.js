c63dd51e519e19c0fa6af742e453b2cf431f304a
LEE DONG GYU <88726990+tenenger@users.noreply.github.com>
2022-11-15 14:46:21 +0900

    Time: 84 ms (90.50%), Space: 42.9 MB (99.66%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.js
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.js
@@ -4,12 +4,5 @@
  */
 var myAtoi = function(s) {
     const num = parseInt(s);
-    return Number.isInteger(num) ? range(num) : 0;
-    
-    
-    function range() {    
-        if (num < -1 * 2 ** 31) return -1 * 2 ** 31;
-        else if (2 ** 31 - 1 < num) return 2 ** 31 - 1;
-        else return num;
-    }
+    return Number.isInteger(num) ? Math.max(-1 * 2 ** 31, Math.min(num, 2 ** 31 - 1)) : 0;
 };
