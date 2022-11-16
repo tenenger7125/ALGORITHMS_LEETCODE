e99f56a78004af1725dbb4b4ca627c53b690d690
LEE DONG GYU <88726990+tenenger@users.noreply.github.com>
2022-11-16 10:13:58 +0900

    Time: 89 ms (86.47%), Space: 48.9 MB (96.87%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+// two pointer로 문제 풀기
+var maxArea = function(height) {
+    let left = 0;
+    let right = height.length - 1;
+    let area = 0;
+    let answer = 0;
+    
+    while (left < right) {
+        area = Math.min(height[left], height[right]) * (right - left);
+        answer = Math.max(answer, area);
+        
+        if (height[left] < height[right]) left++;
+        else right--;
+    }
+    
+    return answer;
+};
