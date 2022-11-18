fedf3415357e2e1c0c61251d8cb6b56b9a033965
LEE DONG GYU <88726990+tenenger@users.noreply.github.com>
2022-11-18 10:20:38 +0900

    Time: 91 ms (65.45%), Space: 41.9 MB (81.34%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function(nums1, m, nums2, n) {
+    let nums1ZeroIdx = m + n - 1;
+    let nums1Idx = m - 1;
+    let nums2Idx = n - 1;
+    
+    while (nums1Idx >= 0 && nums2Idx >= 0) {
+        if (nums1[nums1Idx] <= nums2[nums2Idx]) nums1[nums1ZeroIdx--] = nums2[nums2Idx--]
+        else nums1[nums1ZeroIdx--] = nums1[nums1Idx--]
+    }
+    
+    while (nums1Idx >= 0) {
+        nums1[nums1ZeroIdx--] = nums1[nums1Idx--]
+    }
+    
+    while (nums2Idx >= 0) {
+        nums1[nums1ZeroIdx--] = nums2[nums2Idx--]
+    }
+};
