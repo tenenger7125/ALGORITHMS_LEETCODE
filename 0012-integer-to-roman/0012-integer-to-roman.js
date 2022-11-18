ee87ea8e2f3aed42728c4f441ff461626da9ce25
LEE DONG GYU <88726990+tenenger@users.noreply.github.com>
2022-11-18 11:18:09 +0900

    Time: 258 ms (18.17%), Space: 50.6 MB (14.08%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.js
+++ 0012-integer-to-roman/0012-integer-to-roman.js
@@ -3,26 +3,27 @@
  * @return {string}
  */
 var intToRoman = function(num) {
-    const symbolArr = [
-        ['I', 1],
-        ['IV', 4],
-        ['V', 5],
-        ['IX', 9],
-        ['X', 10],
-        ['XL', 40],
-        ['L', 50],
-        ['XC', 90],
-        ['C', 100],
-        ['CD', 400],
-        ['D', 500],
-        ['CM', 900],
-        ['M', 1000],
-    ]
-    const symbolArrLength = symbolArr.length;
+    const symbolObj = {
+        1:'I',
+        4:'IV',
+        5:'V',
+        9:'IX',
+        10:'X',
+        40:'XL',
+        50:'L',
+        90:'XC',
+        100:'C',
+        400:'CD',
+        500:'D',
+        900:'CM',
+        1000:'M'
+    }
+    const symbolObjEntries = Object.entries(symbolObj);
+    const symbolObjLength = symbolObjEntries.length;
     let answer = '';
     
-    for (let i=symbolArrLength-1; i>=0; i--) {
-        const [symbol, value] = symbolArr[i];
+    for (let i=symbolObjLength-1; i>=0; i--) {
+        const [value, symbol] = symbolObjEntries[i];
         const count = num / value;
         if (count > 0) {
             answer += symbol.repeat(count);
