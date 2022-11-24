e7ac006a0a8c36e271db83f934ae2fd9562b0b52
LEE DONG GYU <88726990+tenenger@users.noreply.github.com>
2022-11-24 16:55:46 +0900

    Time: 127 ms (76.88%), Space: 44.2 MB (73.32%) - LeetHub

--- 0018-4sum/0018-4sum.js
+++ 0018-4sum/0018-4sum.js
@@ -6,36 +6,41 @@
 var fourSum = function(nums, target) {
     const numsLength = nums.length;
     const sortNums = nums.sort((a,b)=>a-b);
-    const set = new Set();
+    const answer = [];
     
     for (let i=0; i<numsLength-3; i++) {
+        if (i > 0 && sortNums[i-1] === sortNums[i]) continue
         
         for (let j=i+1; j<numsLength-2; j++) {
+            if (j > i+1 && sortNums[j-1] === sortNums[j]) continue
+            
             let left = j+1;
             let right = numsLength-1;
             
             while (left < right) {
                 const sum = sortNums[i] + sortNums[j] + sortNums[left] + sortNums[right];
                 
+                
                 if (sum < target) {
+                    left++;    
+                } else if (sum > target) {
+                    right--;
+                } else {
+                    answer.push([sortNums[i], sortNums[j], sortNums[left], sortNums[right]]);
                     left++;
+                    right--;
                     
                     while (sortNums[left] === sortNums[left - 1]) {
                        left++;
                     }
-                } else if (sum > target) {
-                    right--;
                     
                     while (sortNums[right] === sortNums[right + 1]) {
                         right--;
                     }
-                } else {
-                    set.add(JSON.stringify([sortNums[i], sortNums[j], sortNums[left], sortNums[right]]));
-                    left++;
                 }
             }
         }
     }
     
-    return [...set].map(json => JSON.parse(json))
+    return answer
 };
