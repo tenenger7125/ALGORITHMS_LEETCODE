e8dd951767c8dfa20116c1080f49d898015436b7
LEE DONG GYU <88726990+tenenger7125@users.noreply.github.com>
2022-11-29 10:20:37 +0900

    Time: 84 ms (96.64%), Space: 52.1 MB (15.15%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -3,11 +3,12 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
+    const length = prices.length;
     let buyIdx = 0;
     let sellIdx = 1;
     let maxProfit = 0;
     
-    while (sellIdx < prices.length) {
+    while (sellIdx < length) {
         if (prices[buyIdx] < prices[sellIdx]) {
             const profit = prices[sellIdx] - prices[buyIdx];
             
