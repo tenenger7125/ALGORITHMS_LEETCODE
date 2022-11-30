87624ea3a116695608939091df66f4e0a8a8146a
LEE DONG GYU <88726990+tenenger7125@users.noreply.github.com>
2022-11-30 10:38:36 +0900

    Time: 169 ms (5.05%), Space: 45 MB (49.20%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+    const visit = [];
+    let currentNode = head;
+    
+    while (!visit.includes(currentNode) && currentNode !== null) {
+        visit.push(currentNode)
+        currentNode = currentNode.next;
+    }
+    console.log(currentNode)
+    return currentNode !== null
+};
