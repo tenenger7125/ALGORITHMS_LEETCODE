fb36b1884e0335a0034302b3177dc3eebc4560ba
LEE DONG GYU <88726990+tenenger7125@users.noreply.github.com>
2022-12-06 10:38:18 +0900

    Time: 99 ms (75.28%), Space: 45.8 MB (5.06%) - LeetHub

---
+++ 0171-excel-sheet-column-number/0171-excel-sheet-column-number.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} columnTitle
+ * @return {number}
+ */
+var titleToNumber = function(columnTitle) {
+    const length = columnTitle.length;
+    let multiple = 1;
+    let answer = 0;
+    
+    for (let i=length-1; i>=0; i--) {
+        const columnNumber = columnTitle.charCodeAt(i) - 64;
+        multiple = (i === length - 1) ? 1 : multiple * 26;
+        answer += columnNumber * multiple
+        console.log(answer, columnNumber, multiple)
+    }
+    
+    return answer
+};
