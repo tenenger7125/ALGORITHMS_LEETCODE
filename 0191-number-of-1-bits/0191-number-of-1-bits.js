cb61877579c56caf615f990c1a5b43e7e8f0469e
LEE DONG GYU <88726990+tenenger7125@users.noreply.github.com>
2022-12-07 10:33:51 +0900

    Time: 97 ms (66.79%), Space: 42.5 MB (50.07%) - LeetHub

---
+++ 0191-number-of-1-bits/0191-number-of-1-bits.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} n - a positive integer
+ * @return {number}
+ */
+var hammingWeight = function(n) {
+    let count = 0;
+    
+    while (n) {
+        if (n & 1 === 1) count++;
+        n >>>= 1;
+    }
+    
+    return count
+};
