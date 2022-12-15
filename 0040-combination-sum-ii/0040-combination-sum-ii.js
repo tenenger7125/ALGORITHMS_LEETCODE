6eb97d089e454f8233d74210af64bcfc89319ffb
LEE DONG GYU <88726990+tenenger7125@users.noreply.github.com>
2022-12-15 13:14:38 +0900

    Time: 79 ms (88.94%), Space: 44.3 MB (62.50%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.js
+++ 0040-combination-sum-ii/0040-combination-sum-ii.js
@@ -4,26 +4,20 @@
  * @return {number[][]}
  */
 var combinationSum2 = function(candidates, target) {
-    const n = candidates.length;
     const answer = [];
     
     function dfs(temp, idx, sum) {
-        if (sum === target) {
-            console.log('answer:', temp)
-            answer.push(temp.slice())
-        }
+        if (sum === target) return answer.push(temp.slice());
         
-        for (let i=idx; i<n; i++) {
+        for(let i=idx; i<candidates.length; i++) {
             if (sum + candidates[i] > target) return;
             if (i > idx && candidates[i] === candidates[i-1]) continue;
             
-            dfs([...temp, candidates[i]], i+1, sum + candidates[i])
-            
+            dfs([...temp, candidates[i]], i+1, sum + candidates[i]);
         }
     }
-    
-    candidates.sort((a,b)=>a-b);
+    candidates.sort((a,b)=>a-b)
     dfs([], 0, 0)
     
-    return answer
+    return answer;
 };
